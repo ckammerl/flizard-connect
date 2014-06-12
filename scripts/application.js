5fb73c4fc4934f0adde8684ca570a65c5f2d8ab7
djohnson0610 <djohnson0610@hotmail.com>
2014-06-12 13:42:05 -0700

    almost finished

--- scripts/application.js
+++ scripts/application.js
@@ -1,13 +1,26 @@
 //******************** MODEL ********************//
-function board(){
+function Board(){
 	this.rows = [[],[],[],[],[],[]];
 	this.columns = [[],[],[],[],[],[],[]];
 	this.players = [];
 }
+Board.prototype = {
+	addPiece: function(column){
+		if (this.columns[column - 1].length >= 6){
+			break // implement this later
+		}
+		else {
+			// var lastColor = "red"
+			var newPiece = new Piece("red")
+			var insertColumn = this.columns[column - 1]
+			insertColumn.push(newPiece)
+			this.rows[insertColumn.length].push(newPiece)
+		}
+	}
+}
 
-
-function piece(){
-	this.color = "";
+function Piece(color){
+	this.color = "color";
 };
 
 
